5ea9b103dd61268a364b0f7bcb56075a83655f9c
[Keshan Sankalpa] <[m.e.k.sankalpa@gmail.com]>
2021-05-11 17:48:41 +0530

    welcome page recreated

---
+++ forms/login.form.php
@@ -0,0 +1,37 @@
+<div class="modal fade " id="login_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content modal-content-login p-3">
+            
+            <div class="log-icon mx-auto d-flex justify-content-center align-items-center mb-2">
+              <i class="fa fa-user" aria-hidden="true"></i>
+            </div>
+               
+            <div class="form-content">
+                 <h3 class="text-center">LOGIN</h3>
+            </div>
+
+            <form>
+              <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter Your Email">
+              </div>
+
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter Your Password">
+              </div>
+             
+              <div class="text-center mt-4">
+                    <a href="" class="btn btn-primary  login-button">LOGIN</a>
+                    <p>Do not have an account?</p>
+                    <a href="" class="">Signup</a> 
+
+              </div>
+                
+            </form>
+                 
+             
+              
+          </div>
+        </div>
+      </div>
