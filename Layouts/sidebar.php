dc9069a1f6b4f41e4fb15fef1914825e0757cd49
Jnadeesha <jananinadeesha3@gmail.com>
2021-06-27 16:52:17 +0530

    Create Post

--- Layouts/sidebar.php
+++ Layouts/sidebar.php
@@ -16,6 +16,11 @@
                     </a>
                 </li>
                 <li>
+                    <a class="sidebar-a" href="../backend/create_post/post_vehicals.php">
+                        </i class="fa fa-tv fa-fw px-4 nav-icon" aria-hidden="true"><span>Create Posts</span>
+                    </a>
+                </li>
+                <li>
                     <a class="sidebar-a" href="#"><i class="fa fa-user fa-fw px-4 nav-icon" aria-hidden="true">
                         </i><span>Clients</span>
                     </a>
